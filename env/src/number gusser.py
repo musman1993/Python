e388cc5de5a2444485247feebb885669b9a01508
Muhammad Usman <92249894+musman1993@users.noreply.github.com>
2023-05-08 15:04:19 +0500

    double digit game now

--- env/src/number gusser.py
+++ env/src/number gusser.py
@@ -12,7 +12,7 @@ else:
     print('Please type a number next time.')
     quit()
 
-random_number = random.randint(0, top_of_range)
+random_number = random.randint(000, top_of_range)
 guesses = 0
 
 while True:
